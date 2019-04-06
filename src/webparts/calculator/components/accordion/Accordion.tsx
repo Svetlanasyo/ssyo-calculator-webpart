4b280888407f008a82c76c52abc3ff50cb41ab75
Svetlana.Syo <svetlana.syo@gmail.com>
2019-04-06 21:27:59 +0200

    add accordion animation

--- src/webparts/calculator/components/accordion/Accordion.tsx
+++ src/webparts/calculator/components/accordion/Accordion.tsx
@@ -1,10 +1,33 @@
 import * as React from 'react';
 import { IAccordionProps } from './IAccordionProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import * as jQuery from 'jquery';
+import 'jqueryui';
+import { SPComponentLoader } from '@microsoft/sp-loader';
+
+
+
 
 export class Accordion extends React.Component<IAccordionProps> {
 
+    public componentDidMount() {
+
+        SPComponentLoader.loadCss('//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css');
+
+        const accordionOptions: JQueryUI.AccordionOptions = {
+            animate: true,
+            collapsible: false,
+            icons: {
+              header: 'ui-icon-circle-arrow-e',
+              activeHeader: 'ui-icon-circle-arrow-s'
+            }
+          };
+        
+          jQuery('.accordion').accordion(accordionOptions);
+    }
+
   public render(): React.ReactElement<IAccordionProps> {
+
     return (
               <div className="accordion">
                 <h3>Wynik 1</h3>
